6ad0c55689d909c1997ce16d4d683246819209c8
adhithyababu <babuadhithya4@gmail.com>
2026-05-08 10:50:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://placetrack-frontend.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
